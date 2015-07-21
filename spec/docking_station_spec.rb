30aff8f77fb0f6ad698e3d4c2714a9a956f07fb4
Richard Charman <richardcharman@live.com>
2015-07-21 12:22:44 +0000

    Added challenge 9 exercises

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,5 @@
 # spec/docking_station_object.rb
 require 'docking_station'
 describe DockingStation do
-  it 'responds to release_bike' do
-    expect(subject).to respond_to :release_bike
-  end
+  it { is_expected.to respond_to :release_bike }
 end
